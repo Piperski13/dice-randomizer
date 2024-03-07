cd0cc9aeaf6302bb866faecc68062746c346f589
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-03-07 09:56:14 +0100

    Version 1

---
+++ index.js
@@ -0,0 +1,26 @@
+//random number generator
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+//images variables
+var image1 = "images/dice"+randomNumber1+".png"
+var image2 = "images/dice"+randomNumber2+".png"
+
+//DOM manipulation
+document.querySelector(".dice .img1").setAttribute("src",image1);
+document.querySelector(".dice .img2").setAttribute("src",image2);
+
+//Finding the winner
+if(image1>image2){
+    document.querySelector("h1").innerHTML= "Player one has won!"
+}
+else if (image1<image2){
+    document.querySelector("h1").innerHTML= "Player two has won!"
+    document.querySelector("h1").style.fontSize = "7rem"
+
+}
+else if (image1=image2){
+
+    document.querySelector("h1").style.fontSize = "6rem"
+    document.querySelector("h1").innerHTML= "Its a TIE, refresh me again!"
+}
